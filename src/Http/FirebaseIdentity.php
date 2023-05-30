4611ec299c6a69006a4f8a8fec8a28cc1fa7ee15
Michael Slowik <slowik.michael@gmail.com>
2023-05-30 13:38:32 +0200

    FirebaseIdentity model

---
+++ src/Http/FirebaseIdentity.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Firevel\FirebaseAuthentication;
+
+use Firevel\FirebaseAuthentication\FirebaseAuthenticable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class FirebaseIdentity extends Authenticatable
+{
+    use FirebaseAuthenticable;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * Save blocker.
+     *
+     * @param  array  $options
+     * @return bool
+     */
+    public function save(array $options = [])
+    {
+        return true;
+    }
+
+    /**
+     * Get User by claim.
+     *
+     * @param  array  $claims
+     * @return self
+     */
+    public function resolveByClaims(array $claims): object
+    {
+
+        $attributes = $this->transformClaims($claims);
+        $attributes['id'] = (string) $claims['sub'];
+
+        return $this->fill($attributes);
+    }
+
+    
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+}
